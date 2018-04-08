d66d98b28b79fbf3d50da588f3a42890ee282717
qc.hcolde <hhy@gzqichang.com>
2018-04-08 09:36:47 +0800

    remove some mark

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
 	path('<int:page>/', views.PageView, name = 'page'),
 	path('<int:pk>/detail/', views.DetailView.as_view(), name = 'detail'),
 	path('post/', views.PostView, name = 'post_new'),
-	#path('[int:pk]/remove/', views.RemoveView.as_view(), name = 'remove'),
-	#path('[int:pk]/modify/', views.ModifyView.as_view(), name = 'modify'),
 ]
